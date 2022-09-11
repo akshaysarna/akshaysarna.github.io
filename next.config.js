c8cc6dbc959e9c22cc5e501539e7a05019475902
akshaysarna <akshay.sarna01@gmail.com>
2022-09-11 18:20:31 +0530

    unoptimized image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   swcMinify: true,
-}
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      //'/about': { page: '/about' },
+      //'/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
